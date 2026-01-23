3caf53db9783d753edb1967f3215b8c160f19b6f
Tate-Smith <tatelansing@gmail.com>
2026-01-22 20:12:52 -0800

    added some setup

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,19 +1,92 @@
 package Model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Player {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column()
-    private String name;
-    @Column()
+    @Column(nullable = false, unique = true)
+    private int externalId;
+    @Column(nullable = false)
+    private String fullName;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Position position;
-    @Column
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
-    @Column()
-    private int number;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Player(int id, int externalId, String fullName, Position position, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.externalId = externalId;
+        this.fullName = fullName;
+        this.position = position;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(int externalId) {
+        this.externalId = externalId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
